44270ef2bb801ddaef7295c9a9023c725b61ab75
Jonathan Åkerblom <jonathan.akerblom@ffcg.se>
2016-10-21 15:13:09 +0200

    Added kitten cart functionality, roughly. Just inserted codepen in the home view basically.

--- KittenShop/Controllers/HomeController.cs
+++ KittenShop/Controllers/HomeController.cs
@@ -5,12 +5,17 @@ using System.Web.Mvc;
 
 namespace KittenShop.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
+
+        //public ActionResult Help()
+        //{
+        //    return View();
+        //}
     }
 }
