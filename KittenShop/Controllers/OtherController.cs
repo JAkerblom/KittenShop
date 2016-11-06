dc4dedf323fda8fe3cda7785d91e83d057c14a07
Jonathan Åkerblom <jonathan.akerblom@ffcg.se>
2016-11-06 18:13:40 +0100

    Added new help view

---
+++ KittenShop/Controllers/OtherController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace KittenShop.Controllers
+{
+    //[Authorize]
+    public class OtherController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult Help()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ KittenShop/Models/KittenModels/Kitten.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KittenShop.Models.KittenModels
+{
+    public class Kitten
+    {
+    }
+}
